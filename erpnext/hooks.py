2bf28e720ecbfa9883073a0364a0631bcde9e9f0
JulianQuirozG <julianquga@ufps.edu.co>
2024-09-27 16:50:23 +0000

    Encuesta satisfaccion del usuario

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -656,3 +656,7 @@ default_log_clearing_doctypes = {
 export_python_type_annotations = True
 
 fields_for_group_similar_items = ["qty", "amount"]
+
+fixtures = [
+    {"doctype": "DocType", "filters": [["name", "=", "Encuesta de Satisfaccion"]]}
+]
